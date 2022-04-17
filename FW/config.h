f0213d422dfea955b90571845cd57fc8ac1f0d30
tileissner <tleissner.schule@googlemail.com>
2022-04-17 18:19:41 +0200

    add FW

---
+++ FW/config.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID 0xFEED
+#define PRODUCT_ID 0x0002
+#define DEVICE_VER 0x0001
+#define PRODUCT Khada
+
+/* key matrix size */
+#define MATRIX_ROWS 8
+#define MATRIX_COLS 6
+
+#define MATRIX_ROW_PINS { A3, A4, A5, A6 }
+#define MATRIX_COL_PINS { A15, B3, B4, B5, B6, B7 }
+// the defs above were exactly the same as the ones below
+//#define MATRIX_ROW_PINS_RIGHT  { A6, A5, A4, A3 }
+//#define MATRIX_COL_PINS_RIGHT { B7, B6, B5, B4, B3, A15 }
+
+#define DIODE_DIRECTION COL2ROW
+
+#define USE_SERIAL
+#define SOFT_SERIAL_PIN A2
+#define SELECT_SOFT_SERIAL_SPEED 1
+
+#define MATRIX_IO_DELAY 5
+#define TAP_CODE_DELAY 10
+
+#define ENCODERS_PAD_A { B8 }
+#define ENCODERS_PAD_B { B9 }
+
+#define WS2812_PWM_DRIVER PWMD3
+#define WS2812_PWM_CHANNEL 4
+#define WS2812_PWM_PAL_MODE 2
+#define WS2812_DMA_STREAM STM32_DMA1_STREAM2
+#define WS2812_DMA_CHANNEL 5
+
+/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
+#define DEBOUNCE 5
+
+/* disable these deprecated features by default */
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
+
+#define BOOTMAGIC_LITE_ROW	0
+#define BOOTMAGIC_LITE_COLUMN	0
+
+#define BOOTMAGIC_LITE_ROW_RIGHT	4
+#define BOOTMAGIC_LITE_COLUMN_RIGHT	5
---
+++ FW/mcuconf.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include_next <mcuconf.h>
+
+//#undef STM32_I2C_USE_I2C1
+//#define STM32_I2C_USE_I2C1 TRUE
+
+//#undef STM32_PWM_USE_TIM3
+//#define STM32_PWM_USE_TIM3 TRUE
+
+#undef STM32_SERIAL_USE_USART1
+#define STM32_SERIAL_USE_USART1 TRUE
